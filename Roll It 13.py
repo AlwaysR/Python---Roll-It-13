1485f1e2f2c8a3787f25c43341a9dd4c12f41a85
AlwaysR <evansr@mywaicol.nz>
2026-02-23 20:21:41 +1300

    Update Roll It 13.py
    Adding more such as: 
      functions:
        "Yes/No" checker
        "Instructions" printer
        "Integer" checker

--- Roll It 13.py
+++ Roll It 13.py
@@ -1,10 +1,44 @@
-while True:
-    answer = input('Do you want to see the instructions? ').lower()
-    if answer == 'yes' or answer == 'y' or answer == 'yea' or answer == 'yeah' or 'yes' in answer:
-        print('Yes')
-        break
-    elif answer == 'no' or answer == 'n' or answer == 'nah' or 'no' in answer:
-        print('No')
-        break
-    else:
-        print('Please enter yes or no')
+#Functions Here:
+
+def yes_no(question):
+
+    while True:
+
+        """Takes yes no question and gets input"""
+        
+        answer = input(question).lower()
+        if answer == 'yes' or answer == 'y' or answer == 'yea' or answer == 'yeah' or 'yes' in answer:
+           return "yes"
+        elif answer == 'no' or answer == 'n' or answer == 'nah' or 'no' in answer:
+           return "no"
+        else:
+            print('Please enter yes or no')
+
+def instructions():
+
+    print("""
+*** Instructions ***
+
+Roll the dice and try to win!
+""")
+
+def integer_checker(minimun, message):
+    while True:
+        try:
+            response = int(input(message))
+            if response >= minimun:
+                return response
+            else:
+                print(f"Please enter a number equal to or over {minimun}")
+        except ValueError:
+            print("Please enter a valid whole number.")
+
+#Main Code Here:
+
+wants_instructions = yes_no('Do you want instructions? ')
+
+if wants_instructions == "yes":
+    instructions()
+
+game_goal = integer_checker(13, "What is the game goal? ")
+print(game_goal)
